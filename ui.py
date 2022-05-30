f1fc08af8061134d21bbae07db0fea294500a12c
hrfarmer <58487401+hrfarmer@users.noreply.github.com>
2022-05-29 23:15:15 -0500

    non-functioning support for multiple gamemodes
    tried to add support for multiple gamemodes but doesnt work, need elp

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
+from distutils.file_util import copy_file
 import wx
 import os
-import re
 import tkinter as tk
 from tkinter import filedialog
 from elements import taiko_elements
-from elements import taiko_animated_elements
+from elements import standard_elements
+from elements import mania_elements
+from elements import ctb_elements
 
 class Window(wx.Frame):
 
@@ -63,24 +65,33 @@ class Window(wx.Frame):
             self.skin_list = s.get_skins(self.path)
 
         # Makes dropdown appear
-        self.text = wx.StaticText(self.panel, label = "Choose the skin you will copy from", pos = (5, 55))
-        self.combo = wx.ComboBox(self.panel, choices = self.skin_list, pos=(5, 75))
-        self.text2 = wx.StaticText(self.panel, label = "Choose the skin you will copy to", pos = (5, 100))
-        self.combo2 = wx.ComboBox(self.panel, choices = self.skin_list, pos=(5, 120))
-        self.submit = wx.Button(self.panel, label = "Submit", pos = (5, 155))
+        self.text = wx.StaticText(self.panel, label = "Choose the skin you will copy from", pos = (5, 57))
+        self.combo = wx.ComboBox(self.panel, choices = self.skin_list, pos=(5, 77))
+        self.text2 = wx.StaticText(self.panel, label = "Choose the skin you will copy to", pos = (5, 102))
+        self.combo2 = wx.ComboBox(self.panel, choices = self.skin_list, pos=(5, 122))
+        self.submit = wx.Button(self.panel, label = "Submit", pos = (430, 100))
+        self.standard = wx.CheckBox(self.panel, -1, 'Standard', pos = (315, 74))
+        self.taiko = wx.CheckBox(self.panel, -1, 'Taiko', pos = (315, 94))
+        self.mania = wx.CheckBox(self.panel, -1, 'Mania', pos = (315, 114))
+        self.ctb = wx.CheckBox(self.panel, -1, 'Catch the Beat', pos = (315, 134))
         self.submit.Bind(wx.EVT_BUTTON, self.OnSubmit)
-        self.SetSize((500, 243))
+        self.SetSize((500, 218))
     
     def OnSubmit(self, e):
         source_skin = self.combo.GetValue()
         dest_skin = self.combo2.GetValue()
         source_path = f"{self.path}\\Skins\\{source_skin}"
         destination_path = f"{self.path}\\Skins\\{dest_skin}"
-        s.copy_files(source_path, destination_path)
+        standard_value = self.standard.GetValue()
+        taiko_value = self.taiko.GetValue()
+        mania_value = self.mania.GetValue()
+        ctb_value = self.ctb.GetValue()
+        s.determine_copy(source_path, destination_path, standard_value, taiko_value, mania_value, ctb_value)
     
     def OnBrowse(self, e):
         folder_path = filedialog.askdirectory()
         self.path.SetValue(folder_path)
+        self.OnButton(e)
         
 
 class Skin():
@@ -91,10 +102,25 @@ class Skin():
         for folder in os.listdir(skin_folder):
             skins.append(folder)
         return skins
-    
-    def copy_files(self, source_path, destination_path):
+    # NEED ELP
+    def determine_copy(self, source_path, destination_path, standard_value, taiko_value, ctb_value, mania_value):
+        if standard_value:
+            self.copy_files(source_path, destination_path, standard_elements)
+            standard_value = False
+        elif taiko_value:
+            self.copy_files(source_path, destination_path, taiko_elements)
+            taiko_value = False
+        elif ctb_value:
+            self.copy_files(source_path, destination_path, ctb_elements)
+            ctb_value = False
+        elif mania_value:
+            self.copy_files(source_path, destination_path, mania_elements)
+            mania_value = False
+
+
+    def copy_files(self, source_path, destination_path, elements):
         for file in os.listdir(source_path):
-            for element in taiko_elements:
+            for element in elements:
                 file = file.replace(".png", "").strip()
                 element = element.replace(".png", "").strip()
                 if file.startswith(element):
@@ -109,6 +135,7 @@ class Skin():
                     print(f"Copied {file}")
                 else:
                     continue
+        
 
 s = Skin()
 
